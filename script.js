a08566636b0915a040b3cf209ac2832f8ef5f0fe
aidiel <aidiekamil@gmail.com>
2025-12-20 22:02:16 +0700

    baru

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ semua_tombol.forEach((tombol_satuan) => {
     });
     });
 });
+
+const menuIcon = document.getElementById("menu-icon")
+const menuList = document.getElementById("menu-list")
+
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("hidden")
+   
+})
